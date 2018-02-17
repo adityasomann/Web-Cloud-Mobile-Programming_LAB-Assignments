4f317330eb323b33588c27ca4220566861cd7985
AdarshMergoju <adarshmergoju@gmail.com>
2018-02-17 14:05:16 -0600

    assignment2

--- Assignment -2/News App/Source/script.js
+++ Assignment -2/News App/Source/script.js
@@ -1,9 +1,10 @@
-function getGithubInfo(user) {
+function search() {
     //1. Create an instance of XMLHttpRequest class and send a GET request using it. The function should finally return the object(it now contains the response!)
 
+    var search_item=document.getElementById("username").value;
     return $.ajax({type: "GET",
         url:'https://newsapi.org/v2/everything?' +
-        'q=Apple&' +
+        'q=' + search_item + '&' +
         'from=2018-02-15&' +
         'sortBy=popularity&' +
         'apiKey=35dd737749cf48d98ac68bd6c627be7e',
